160101ab3a2ef5d32bd696bddceb60648ceb9dbe
Aishida Abayomi <yomicoperate@gmail.com>
2020-01-16 10:32:34 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// Search input
+const searchUser = document.getElementById("searchUser");
+
+// Search input event listener
+searchUser.addEventListener("keyup", e => {
+  // Get input text
+  const userText = e.target.value;
+
+  if (userText !== "") {
+    console.log(userText);
+  }
+});
