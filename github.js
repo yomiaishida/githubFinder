1a083bbbd2083a99b87c7de152945d0f8e025ff9
Aishida Abayomi <yomicoperate@gmail.com>
2020-01-21 11:18:55 +0100

    added alerts for when a user is not found and a blank profile when input is empty

--- github.js
+++ github.js
@@ -9,7 +9,7 @@ class Github {
       `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}  `
     );
 
-    const profileData = await profileResponse.json();
+    const profile = await profileResponse.json();
 
     return {
       profile
--- ui.js
+++ ui.js
@@ -3,6 +3,7 @@ class UI {
     this.profile = document.getElementById("profile");
   }
 
+  // Display profile in UI
   showProfile(user) {
     this.profile.innerHTML = `
     <div class="card card-body mb-3">
@@ -29,5 +30,45 @@ class UI {
     <h3 class="page-heading mb-3>Latest Repos</h3>
     <div id="repos"></div>
     `;
+  }
+
+  // Show alert message
+  showAlert(message, className) {
+    // clear any remaining alerts
+    this.clearAlert();
+    // create div
+    const div = document.createElement("div");
+    // Add classes
+    div.className = className;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector(".searchContainer");
+    // Get search box
+    const search = document.querySelector(".search");
+    // Insert alert
+    container.insertBefore(div, search);
+
+    // Timeout after three seconds
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+
+    // Clear profile when there is no corresponding user
+    this.profile.innerHTML = "";
+  }
+
+  // Clear alert message
+  clearAlert() {
+    const currentAlert = document.querySelector(".alert");
+
+    if (currentAlert) {
+      currentAlert.remove();
+    }
+  }
+
+  // Clear Profile
+  clearProfile() {
+    this.profile.innerHTML = "";
   }
 }
